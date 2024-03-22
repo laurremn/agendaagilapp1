8298b07bc77ffa876579fdf1a68c49827a5d970e
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-03-22 17:26:57 -0500

    formulario

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import { Formulario } from './Formulario/Formulario'
 
 
 import { Home } from './Home/Home' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home></Home>
+    <Formulario></Formulario>
   </React.StrictMode>,
 )
